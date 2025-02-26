b7b8e9f6a3db1d7d044e42cf38caabfc2e3b7884
zedaes <147769903+zedaes@users.noreply.github.com>
2025-02-26 00:06:04 -0500

    feat: basic SDL2 window

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+int main() {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow("Ray Tracer",
+                                          SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                                          800, 600, SDL_WINDOW_SHOWN);
+
+    if (!window) {
+        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Delay(2000);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
